ecf2289c591347d3801a2e975292b5d278a459a2
joywt <11@qq.com>
2021-05-14 11:12:24 +0800

    img

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.caixin.com%2F2020-03-28%2F1585373533147178_480_320.jpg&refer=http%3A%2F%2Fimg.caixin.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1623553869&t=ca2bcf1fdb012e848849d28bc8c17b3d">
+    <HelloWorld msg="Are you serious!!"/>
   </div>
 </template>
 
